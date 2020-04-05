ed126376b3564f9a17c0f1f17b847458fcc5a0f4
Jo8333 <63186696+Jo8333@users.noreply.github.com>
2020-04-05 16:18:19 +0200

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be used to Capstone project
+
+# In[10]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[11]:
+
+
+print("Hello Capstone Project Course!")
+
